9bffd9edf6ba17dc7ec618517647f7ad027f867f
Carlo <carlo.namoca@gmail.com>
2017-10-20 14:15:13 -0700

    Initial Commit

---
+++ AV Storybook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AV Storybook
+//
+//  Created by Carlo Namoca on 2017-10-20.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AV StorybookTests/AV_StorybookTests.m
@@ -0,0 +1,39 @@
+//
+//  AV_StorybookTests.m
+//  AV StorybookTests
+//
+//  Created by Carlo Namoca on 2017-10-20.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AV_StorybookTests : XCTestCase
+
+@end
+
+@implementation AV_StorybookTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
